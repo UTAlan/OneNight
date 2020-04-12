41d3a86e04247fe3d07ca8f6c1a75e2b12d644d9
Alan Beam <abeam-intricity@users.noreply.github.com>
2020-04-11 19:40:03 -0500

    Fix Express for Heroku

--- index.js
+++ index.js
@@ -253,9 +253,7 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 	
 const shuffle = (array) => {
 	array.sort(() => Math.random() - 0.5);
